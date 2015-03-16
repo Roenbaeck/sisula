57c58d0640d9968b6c38851a942bf0deb02dbfcf
Lars Rönnbäck <lars.ronnback@anchormodeling.com>
2015-03-16 14:14:08 +0100

    Fixed wrong path to Example data
    Fixed a path reference that was pointing towards the old location of the
    example data.

--- Example/sources/source.sql
+++ Example/sources/source.sql
@@ -10,7 +10,7 @@ GO
 -- the target of the BULK INSERT operation, since it cannot insert
 -- into a table with multiple columns without a format file.
 --
--- Generated: Mon Mar 16 13:21:26 UTC+0100 2015 by e-lronnback
+-- Generated: Mon Mar 16 13:26:21 UTC+0100 2015 by e-lronnback
 -- From: TSE-9B50TY1 in the CORPNET domain
 --------------------------------------------------------------------------
 CREATE PROCEDURE [NYPD_Vehicle_CreateInsertView] (
@@ -80,7 +80,7 @@ GO
 -- This job may called multiple times in a workflow when more than
 -- one file matching a given filename pattern is found.
 --
--- Generated: Mon Mar 16 13:21:26 UTC+0100 2015 by e-lronnback
+-- Generated: Mon Mar 16 13:26:21 UTC+0100 2015 by e-lronnback
 -- From: TSE-9B50TY1 in the CORPNET domain
 --------------------------------------------------------------------------
 CREATE PROCEDURE [NYPD_Vehicle_BulkInsert] (
@@ -192,7 +192,7 @@ GO
 -- Create: NYPD_Vehicle_Collision_Split
 -- Create: NYPD_Vehicle_CollisionMetadata_Split
 --
--- Generated: Mon Mar 16 13:21:26 UTC+0100 2015 by e-lronnback
+-- Generated: Mon Mar 16 13:26:21 UTC+0100 2015 by e-lronnback
 -- From: TSE-9B50TY1 in the CORPNET domain
 --------------------------------------------------------------------------
 CREATE PROCEDURE [NYPD_Vehicle_CreateSplitViews] (
@@ -468,7 +468,7 @@ GO
 -- Create: NYPD_Vehicle_Collision_Error
 -- Create: NYPD_Vehicle_CollisionMetadata_Error
 --
--- Generated: Mon Mar 16 13:21:26 UTC+0100 2015 by e-lronnback
+-- Generated: Mon Mar 16 13:26:21 UTC+0100 2015 by e-lronnback
 -- From: TSE-9B50TY1 in the CORPNET domain
 --------------------------------------------------------------------------
 CREATE PROCEDURE [NYPD_Vehicle_CreateErrorViews] (
@@ -578,7 +578,7 @@ GO
 -- Create: NYPD_Vehicle_Collision_Typed
 -- Create: NYPD_Vehicle_CollisionMetadata_Typed
 --
--- Generated: Mon Mar 16 13:21:26 UTC+0100 2015 by e-lronnback
+-- Generated: Mon Mar 16 13:26:21 UTC+0100 2015 by e-lronnback
 -- From: TSE-9B50TY1 in the CORPNET domain
 --------------------------------------------------------------------------
 CREATE PROCEDURE [NYPD_Vehicle_CreateTypedTables] (
@@ -682,7 +682,7 @@ GO
 -- Load: NYPD_Vehicle_Collision_Split into NYPD_Vehicle_Collision_Typed
 -- Load: NYPD_Vehicle_CollisionMetadata_Split into NYPD_Vehicle_CollisionMetadata_Typed
 --
--- Generated: Mon Mar 16 13:21:26 UTC+0100 2015 by e-lronnback
+-- Generated: Mon Mar 16 13:26:21 UTC+0100 2015 by e-lronnback
 -- From: TSE-9B50TY1 in the CORPNET domain
 --------------------------------------------------------------------------
 CREATE PROCEDURE [NYPD_Vehicle_SplitRawIntoTyped] (
@@ -846,7 +846,7 @@ GO
 -- Key: CrossStreet (as primary key)
 -- Key: CollisionOrder (as primary key)
 --
--- Generated: Mon Mar 16 13:21:26 UTC+0100 2015 by e-lronnback
+-- Generated: Mon Mar 16 13:26:21 UTC+0100 2015 by e-lronnback
 -- From: TSE-9B50TY1 in the CORPNET domain
 --------------------------------------------------------------------------
 CREATE PROCEDURE [NYPD_Vehicle_AddKeysToTyped] (
